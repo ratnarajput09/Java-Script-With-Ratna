a3b587607d3acbe7d6b8670444ae3297e132e07b
Rajput Ratna <108252978+ratnarajput09@users.noreply.github.com>
2024-02-15 18:13:11 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+/*alert(3+3) 
+we are using nodejs not browser.
+*/
+
+//js automatically ignores the space.
+
+//code reability shoud be high.
+
+//ecma and mdn to read documantation.
+/* datatypes in js
+    primitive types:-
+    number
+    bigint
+    string = ""
+    boolean = either true or false.
+    null = standalone value(represent of empty value.)
+    undefined = when you dont assign value.
+    symbol = unique
+*/
+
+/*
+*/
