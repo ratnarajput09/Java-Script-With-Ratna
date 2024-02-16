04f8b6800f723a0ae99ac6961448504814bbd71c
Rajput Ratna <108252978+ratnarajput09@users.noreply.github.com>
2024-02-16 09:14:18 +0000

    conversion of datatypes

---
+++ 01_basic/conversion operarion.js
@@ -0,0 +1,27 @@
+let value = "33";
+
+console.log(typeof (value));
+
+let valueInNumber = Number(value)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//NaN is not a number.
+
+/*
+conversion
+"33" => 33
+"33abc" => NaN
+true => 1
+false => 0
+"" , '' => false
+"ratna" , "33"=> true
+*/
+
+let isloggedIn = 1
+console.log(typeof(isloggedIn));
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+ 
