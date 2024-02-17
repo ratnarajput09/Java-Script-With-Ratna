f94b297b6676c8382b089680f78d71e9c412039d
Rajput Ratna <108252978+ratnarajput09@users.noreply.github.com>
2024-02-17 20:26:30 +0000

    strings in javascript

---
+++ 01_basic/string.js
@@ -0,0 +1,16 @@
+//console.log("ratna" + " hello");
+//this you should not use to concat.
+
+console.log(`${"hello"} ${"ratna"}`);
+//this is mordern way to declare and concate.This is called string interpolation.
+//if you directly concate the strings you cannot use string.methods with them for that you have to declare them first in variable.
+
+
+//there is no difference in op of both.
+
+
+//String declaration 2 ways.
+
+let superman = "tarzan"
+
+let Superman = new String('tarzan');
