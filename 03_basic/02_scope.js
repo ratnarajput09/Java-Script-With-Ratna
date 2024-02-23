af90c5721ac24f77b71d4fbd78784937aaef7263
Rajput Ratna <108252978+ratnarajput09@users.noreply.github.com>
2024-02-23 10:12:12 +0000

    scope of function and variables

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,60 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+//function declaration way 
+
+//1st way
+console.log(addone(5))//give you output
+
+function addone(num){
+    return num + 1
+}
+
+
+//2ndway
+addTwo(5) //give you error
+const addTwo = function(num){
+    return num + 2
+}
