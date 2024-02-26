38b794e2004920183af75953d14b6b78ab5b8f54
Rajput Ratna <108252978+ratnarajput09@users.noreply.github.com>
2024-02-26 05:51:07 +0000

    example of truthy and falshy values.

--- 04_controlflow/truthy_falsy.js
+++ 04_controlflow/truthy_falsy.js
@@ -4,3 +4,22 @@
 
 //truthy values
 // "0", 'false', " ", [], {}, function(){}
+
+
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
