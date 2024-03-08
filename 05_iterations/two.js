4bc219b52f07ebb113878f071de87d2c20c3b82a
Rajput Ratna <108252978+ratnarajput09@users.noreply.github.com>
2024-03-08 06:52:10 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+//while loop
+
+let index = 0
+while (index <= 10) {
+   // console.log(index);
+    index++
+}
+
+//array in while loop
+
+let myarray = ['flash','spiderman','black-panthar','batman']
+let arr = 0
+while (arr < myarray.length) {
+    //console.log(myarray[arr]);
+    arr++
+}
+
+
+//do-while loop
+
+
+let index1 = 0
+do {
+    console.log(`the number is ${index1}`);
+    index1++
+} while (index1 <= 10);
