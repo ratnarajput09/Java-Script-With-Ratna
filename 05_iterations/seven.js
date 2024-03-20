96e7308078e07fe45caf979c47cbd7f07ca86283
Rajput Ratna <108252978+ratnarajput09@users.noreply.github.com>
2024-03-20 10:42:47 +0000

    map and more than two function used in one program

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -3,4 +3,11 @@
 const myNums = [1,2,3,4,5,6,7,8]
 
 const newNums = myNums.map((num) => num -1)
-console.log(newNums);
+//console.log(newNums);
+
+
+const newnums = myNums
+.map( (nums) => nums*10 +1)
+.filter( (nums) => nums>=20)
+
+console.log(newnums);
